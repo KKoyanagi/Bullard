f4c9f43f664f8840c27246b5b546440c5d36541d
donald <donemurch@gmail.com>
2017-03-05 17:03:52 -0800

    Added ProjectRepository

---
+++ Bullard Inc/Bullard Inc/Models/ActivityCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bullard_Inc.Models
+{
+    public class ActivityCode
+    {
+        [Key]
+        public int ActivityCode_Id { get; set; }
+        [StringLength(50)]
+        public string ActivityDescription { get; set; }
+    }
+}
